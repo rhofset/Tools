169a0d7331e639acaf80cb95df8be5b3ad6b4b3b
rhofset <38030025+rhofset@users.noreply.github.com>
2022-01-16 11:55:55 +0100

    Update ping.py
    Added hostnames to found IP's

--- network/ping.py
+++ network/ping.py
@@ -5,6 +5,7 @@ import concurrent.futures
 from datetime import datetime
 import functools
 from netaddr import IPNetwork
+import socket
 import subprocess
 import platform
 from tqdm import tqdm
@@ -21,13 +22,22 @@ def ping(ip):
     else: 
         not_ok_ip_addr.append(ip) # This is not in use at this moment
 
+def host(ip):
+    not_found = ['Hostname not found']
+    try:
+        host_name = socket.gethostbyaddr(str(ip))
+        return host_name
+    except:
+        host_name = list(not_found)
+        return host_name
+
 def hostaddr(ip):
     ip_list = []
     for host in IPNetwork(ip):
         ip_list.append(host)
         f = []
     with concurrent.futures.ThreadPoolExecutor(254) as executor: # Change the number to how many threads you want.
-        for i in tqdm(ip_list):
+        for i in ip_list:
             f.append([(host, executor.submit(functools.partial(ping, i)))])
 
 if __name__ == "__main__":
@@ -37,8 +47,12 @@ if __name__ == "__main__":
 
     hostaddr(args.ipsubnet)
     print("\n")
-    for i in sorted(ok_ip_addr):
+    sorted_list = sorted(ok_ip_addr)
+    sorted_list_host = []
+    for i in tqdm(sorted_list):
+        sorted_list_host.append(f"{i}, {host(i)[0]}")
+    print("\n")
+    for i in sorted_list_host:
         print(i)
     print("\n")
     print(datetime.now() - startTime)
-    
